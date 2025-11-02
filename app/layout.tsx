7f1ab0c6c06c7fa9fc174546393677a09453900a
mini-chip <kimmin5209@gmail.com>
2025-11-02 10:09:50 +0900

    feat: layout.tsx 설정#4

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
   description: '간단한 설문으로 비슷한 시기, 같은 지역으로 떠나는 사람들을 자동으로 매칭해드려요. 매칭된 그룹에서는 정착 준비·정보 공유·소모임 일정까지 함께 나눌 수 있어요',
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
