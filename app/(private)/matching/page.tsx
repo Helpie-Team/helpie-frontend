4e6c0ce485c4e9278e84f8d33e86805d676c552e
mini-chip <kimmin5209@gmail.com>
2025-11-05 00:12:42 +0900

    Style: MatchingBar 컴포넌트 구조 개선 및 검색 기능 추가, 스타일 수정#4

--- app/(private)/matching/page.tsx
+++ app/(private)/matching/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '@/app/components/common/Header/Header';
 import MatchingBar from '@/app/components/matching/MatchingBar';
 export default function page() {
   return <div>
--- app/components/matching/MatchingBar.tsx
+++ app/components/matching/MatchingBar.tsx
@@ -1,15 +1,38 @@
 import React from "react"
+import Image from "next/image"
 export default function MatchingBar() {
   return(
-  <div>
-    <div className="flex justify-between items-center w-full h-11 mb-4">
-      <h2 className="text-h2-sb">한국 소모임</h2>
+  <div className="flex flex-col items-center h-[136px] my-8 gap-6">
+    <div className="flex items-center justify-between  w-[1000px] h-11 mb-4">
+      <div className="w-[531px] h-[43px] flex items-center gap-2 text-[32px] font-semibold leading-none text-black">
+        <div className="flex items-center gap-2">
+          <p>전체</p>
+          <Image
+            src="/icons/down.png"
+            alt="dropdown"
+            width={34}
+            height={34}
+            className="cursor-pointer"
+          />
+        </div>
+        <p>소모임</p>
+      </div>
       <button className="flex items-center justify-center w-[119px] h-[43px] px-4 py-3 rounded-[55px] bg-grayScale-700 text-grayScale-white text-body1-sb whitespace-nowrap">소모임 만들기</button>
     </div>
-    <div className="flex w-[1000px] h-11 gap-3">
-      <button className="w-[80px] h-11 rounded-[61px] px-2 py-4 bg-grayScale-filter text-grayScale-black"></button>
-      <input className="flex justify-between items-center w-[908px] h-[44px] py-2 pr-2 pl-3 rounded-full border border-grayScale-filter"></input>
 
+    {/* 검색 입력창 */}
+    <div className="relative w-[1000px] items-center justify-center">
+      <input
+        className="w-full h-[44px] py-2 pl-3 pr-12 rounded-full border border-grayScale-filter text-body2-regular placeholder:text-grayScale-300"
+        placeholder="전체 소모임 검색"
+      />
+      <Image
+        src="/icons/searchIcon.png"
+        alt="search"
+        width={24}
+        height={24}
+        className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+      />
     </div>
   </div>
   )
