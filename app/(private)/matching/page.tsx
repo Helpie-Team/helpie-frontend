f1b0494d8b21312bb266ed9ed4f126a76cbaf61a
mini-chip <kimmin5209@gmail.com>
2025-11-04 19:38:32 +0900

    Refactor: MatchingBar 컴포넌트 스타일 개선 및 불필요한 코드 제거, 글로벌 CSS 변수 추가#4

--- app/(private)/matching/page.tsx
+++ app/(private)/matching/page.tsx
@@ -3,7 +3,6 @@ import Header from '@/app/components/common/Header/Header';
 import MatchingBar from '@/app/components/matching/MatchingBar';
 export default function page() {
   return <div>
-    <Header />
     <MatchingBar />
   </div>;
 }
--- app/components/matching/MatchingBar.tsx
+++ app/components/matching/MatchingBar.tsx
@@ -1,16 +1,15 @@
 import React from "react"
 export default function MatchingBar() {
-  return( 
+  return(
   <div>
-    <div>
-      <h2>한국 소모임</h2>
-      <div>
-        <button>매칭하기</button>
-        <button>소모임 만들기</button>
-      </div>
+    <div className="flex justify-between items-center w-full h-11 mb-4">
+      <h2 className="text-h2-sb">한국 소모임</h2>
+      <button className="flex items-center justify-center w-[119px] h-[43px] px-4 py-3 rounded-[55px] bg-grayScale-700 text-grayScale-white text-body1-sb whitespace-nowrap">소모임 만들기</button>
     </div>
-    <div>
-      
+    <div className="flex w-[1000px] h-11 gap-3">
+      <button className="w-[80px] h-11 rounded-[61px] px-2 py-4 bg-grayScale-filter text-grayScale-black"></button>
+      <input className="flex justify-between items-center w-[908px] h-[44px] py-2 pr-2 pl-3 rounded-full border border-grayScale-filter"></input>
+
     </div>
   </div>
   )
