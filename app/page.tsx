10db48ad12956584bf68340cda6e7a7d60629c53
박지원 <jackgg12322@gmail.com>
2025-11-12 01:26:45 +0900

    feat: 홈 메인 콘텐츠 섹션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import ChatBot from "./components/common/ChatBot/ChatBot";
-
+import MainContent from "./components/domain/home/MainContent";
 export default function Home() {
   return <div className="flex flex-col items-center justify-center">
+    <MainContent />
     <ChatBot />
   </div>;
 }
