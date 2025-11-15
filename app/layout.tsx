e1f000604caf1dc6f3df740162ab05e5f37c6d82
박지원 <jackgg12322@gmail.com>
2025-11-15 21:24:12 +0900

    [QA] "openGraph 설정"

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import Header from './components/common/Header/Header';
 import Footer from './components/common/Footer/Footer';
 import { QueryProvider } from './lib/query/QueryProvider';
-
+import OpenGraphImage from '@/public/images/3D_helpie_3.png';
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
@@ -14,7 +14,20 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: 'Helpie',
   description: '간단한 설문으로 비슷한 시기, 같은 지역으로 떠나는 사람들을 자동으로 매칭해드려요. 매칭된 그룹에서는 정착 준비·정보 공유·소모임 일정까지 함께 나눌 수 있어요',
+
+  openGraph: {
+    title: 'Helpie',
+    description: '간단한 설문으로 비슷한 시기, 같은 지역으로 떠나는 사람들을 자동으로 매칭해드려요. 매칭된 그룹에서는 정착 준비·정보 공유·소모임 일정까지 함께 나눌 수 있어요',
+    images: OpenGraphImage.src,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Helpie',
+    description: '간단한 설문으로 비슷한 시기, 같은 지역으로 떠나는 사람들을 자동으로 매칭해드려요. 매칭된 그룹에서는 정착 준비·정보 공유·소모임 일정까지 함께 나눌 수 있어요',
+    images: OpenGraphImage.src,
+  },
 };
+
 
 
 export default function RootLayout({
